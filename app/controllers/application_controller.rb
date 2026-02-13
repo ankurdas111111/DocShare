18122f13218ecd631ce8e32d8fb0711c67415860
Ankur Das <ankurdas111111@gmail.com>
2026-02-13 16:29:14 +0530

    Installed devise gem for authentication, created user model and ran migrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  # Require login for all pages by default.
+  # Controllers that allow public access (e.g. shared documents) will skip this.
+  before_action :authenticate_user!
 end
