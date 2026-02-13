471318e9ee0413a5b4692e2ab802dfa4df7bba9a
Ankur Das <ankurdas111111@gmail.com>
2026-02-13 16:50:50 +0530

    added active storage pdf attachment with validations

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,30 @@
 class Document < ApplicationRecord
   belongs_to :user
 
+  has_one_attached :file
+
   validates :title, presence: true
+  validates :file, presence: true
+  validate :file_must_be_pdf
+  validate :file_must_be_under_size_limit
+
+  MAX_FILE_SIZE = 10 # megabytes
+
+  private
+
+  def file_must_be_pdf
+    return unless file.attached?
+
+    unless file.content_type == "application/pdf"
+      errors.add(:file, "must be a PDF")
+    end
+  end
+
+  def file_must_be_under_size_limit
+    return unless file.attached?
+
+    if file.byte_size > MAX_FILE_SIZE.megabytes
+      errors.add(:file, "must be less than #{MAX_FILE_SIZE}MB")
+    end
+  end
 end
